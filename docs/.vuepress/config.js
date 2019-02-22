a38037ee73a04daa63ae1565d7f493fd16bfcf8b
François Kerisit <fkerisit@w2next.fr>
2019-02-22 15:04:14 +0100

    Update title and description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "Harry Potter",
-  description: "The description of the site.",
+  title: "François Kerisit",
+  description: "My personal website.",
   head: [["link", { rel: "icon", href: `/logo.png` }]],
   base: "/",
   dest: "./dist",
